b0d005d1897a8585b0a578767fc064050bf801fb
Zelalem Teklu <zelalem@kemercode.com>
2025-03-14 16:17:42 +0300

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,42 @@ export const metadata = {
   title: "FRIENDS Forum - Nile Basin Research & Collaboration",
   description:
     "An international platform for research, knowledge exchange, and development support in the Nile Basin region.",
-    generator: 'v0.dev'
+  icons: {
+    icon: [
+      {
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/favicon-16x16-09cgaQ7Ek2B64KsuZRWeAaMzDZjJ74.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/favicon-32x32-P50wgDLnaMay5RwmPtecsIvUBSaLZA.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/apple-touch-icon-PpbxZSBRA6IJgGWUMzb63zwXhj6cbj.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: [
+      {
+        rel: "android-chrome",
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/android-chrome-512x512-UA5G2v63FETvpsLwOFHIcRYh9tb2NK.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        rel: "android-chrome",
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/android-chrome-192x192-xgpkE8itzByXoY1jTYFauJWxWidg4p.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
 }
 
 export default function RootLayout({
@@ -36,6 +71,3 @@ export default function RootLayout({
   )
 }
 
-
-
-import './globals.css'
